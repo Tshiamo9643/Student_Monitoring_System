98f330ef275a7083ffad20d8754e352ab9bfe413
Tshiamo9643 <127095664+Tshiamo9643@users.noreply.github.com>
2024-09-13 09:59:20 +0200

    Add files via upload

---
+++ student_monitoring/students/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    grade = models.CharField(max_length=10)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# Create your models here.
---
+++ student_monitoring/students/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Student
+from .forms import StudentForm
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_list.html', {'students': students})
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'students/student_detail.html', {'student': student})
+
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            student = form.save()
+            return redirect('student_detail', pk=student.pk)
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/student_edit.html', {'form': form})
+
+# Create your views here.
